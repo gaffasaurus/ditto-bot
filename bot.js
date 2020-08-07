9a6b4feb930209e5d218d03a73fa37dd0f5a5172
gaffasaurus <christopher.yao.27@gmail.com>
2020-08-07 15:07:57 -0700

    added lots

--- bot.js
+++ bot.js
@@ -6,10 +6,20 @@ const fs = require('fs');
 // create a new Discord client
 const client = new Discord.Client();
 const prefix = ">";
+
+const modesList = {
+	'random': { value: false, desc: "Toggle to make Ditto transform randomly without being prompted" },
+	'exclusive': { value: true, desc: "Toggle to only allow transformations into members of the current server" }
+}
+const modes = {};
+
+const defaultRate = 0.03;
+const rates = {};
+
+let availableForms = {};
 // when the client is ready, run this code
 // this event will only trigger one time after logging in
 client.once('ready', async () => {
-	console.log('Ready!');
 	for (let person of people.people) {
 		try {
 			user = await client.users.fetch(person.id);
@@ -24,14 +34,30 @@ client.once('ready', async () => {
 			person.avatar = 'https://cdn.discordapp.com/attachments/711793617529995297/740856220394455100/ditto.png';
 		}
 	}
+	client.guilds.cache.forEach(guild => {
+		modes[guild.id] = deepClone(modesList);
+		guild.channels.cache.forEach(channel => {
+			if (channel.type === "text") {
+				rates[channel.id] = defaultRate;
+			}
+		});
+		availableForms[guild.id] = [];
+		const members = [];
+		guild.members.cache.forEach(member => {
+			members.push(member.id);
+		});
+		for (let person of people.people) {
+			if (members.includes(person.id)) {
+				availableForms[guild.id].push(person.id);
+			}
+		}
+	});
+	console.log('Ready!');
 });
 
 let transformed = {};
 let webhook;
 // const available = [];
-const modes = {
-	'random': { value: false, desc: "Toggle to make Ditto transform randomly without being prompted" }
-}
 
 client.on('message', async message => {
 	if (message.author.bot) return;
@@ -45,13 +71,17 @@ client.on('message', async message => {
   }
   //randomly responds on message
   if (!message.content.startsWith(prefix)) {
-		for (let mode in modes) {
+		for (let mode in modesList) {
 			switch (mode) {
 				case 'random': {
-					if (modes[mode].value) {
+					if (modes[message.guild.id].random.value) {
+						let selected = Math.floor(Math.random() * people.people.length);
+						console.log(availableForms[message.guild.id]);
+						console.log(availableForms[message.guild.id].includes(people.people[selected].id));
 						if (Math.random() < 0.3) {
-							let selected = Math.floor(Math.random() * people.people.length);
-							await transform(people.people[selected], message);
+							if (availableForms[message.guild.id].includes(people.people[selected].id)) {
+								await transform(people.people[selected], message);
+							}
 						}
 					}
 				}
@@ -67,7 +97,7 @@ client.on('message', async message => {
         }
       }
       let selected = Math.floor(Math.random() * available.length);
-      if (Math.random() < currentForm.rate || message.content === "<@!740746928186327072>") { // % chance to respond
+      if (Math.random() < rates[message.channel.id] || message.content === "<@!740746928186327072>") { // % chance to respond
         sendWebhook(available[selected], currentForm.nickname, currentForm.avatar);
         available.splice(selected, 1);
       }
@@ -89,7 +119,8 @@ client.on('message', async message => {
             { name: 'forms', value: 'Displays all available forms' },
 						{ name: 'toggle', value: 'Toggles a mode on/off. Use >modes to see available modes' },
 						{ name: 'modes', value: 'Displays all available modes' },
-						{ name: 'status', value: 'Displays modes that are toggled' }
+						{ name: 'status', value: 'Displays modes that are toggled' },
+						{ name: 'rate', value: 'Input a percentage between 0-100 to set how likely the bot is to respond' }
           );
         message.channel.send(embed);
         break;
@@ -98,7 +129,7 @@ client.on('message', async message => {
         if (transformed[message.guild.id]) {
 					let currentForm = transformed[message.guild.id].person;
           let embed = new Discord.MessageEmbed()
-	         .setTitle('Current Form: ' + currentForm.name)
+	         .setTitle('Current Form: ' + currentForm.username)
            .setDescription('Use ">transform name" to give me another form! See ">forms" for a list of forms.')
            .setThumbnail(currentForm.avatar)
 	         .setColor('#FFC0CB');
@@ -113,23 +144,40 @@ client.on('message', async message => {
         break;
       }
       case 'forms': {
-        const users = [];
-        const guild = client.guilds.cache.get(message.guild.id + "");
-        guild.members.cache.forEach(member => {
-          users.push(member.user);
-        });
-        const valid = [];
-        for (user of users) {
-          for (person of people.people) {
-            if (person.id && person.id === user.id) {
-              valid.push([person.name, "@" + user.username + "#" + user.discriminator]);
-            }
-          }
-        }
+			  const valid = [];
+				const users = [];
+				const guild = client.guilds.cache.get(message.guild.id + "");
+				if (modes[message.guild.id].exclusive.value) {
+	        guild.members.cache.forEach(member => {
+	          users.push(member.user);
+	        });
+	        for (let user of users) {
+	          for (let person of people.people) {
+	            if (person.id && person.id === user.id) {
+	              valid.push("- @" + user.username);
+	            }
+	          }
+	        }
+				} else {
+					for (let person of people.people) {
+						let user;
+						try {
+							user = await client.users.fetch(person.id);
+						} catch {
+							user = "none";
+						}
+						if (user !== "none") {
+							valid.push("@" + user.username);
+						}
+					}
+				}
         let embed = new Discord.MessageEmbed()
           .setTitle("Available Forms (use full name or mention with >transform):")
+					if (modes[message.guild.id].exclusive.value) {
+						embed.setDescription("(Only showing members in " + message.guild.name + ")");
+					}
           valid.forEach(form => {
-            embed.addField(form[0], form[1], true);
+            embed.addField(form, '\u200b', true);
           })
           embed.setColor('#FFC0CB');
         message.channel.send(embed);
@@ -139,20 +187,18 @@ client.on('message', async message => {
 				let embed = new Discord.MessageEmbed()
 					.setTitle("Toggled Modes:")
 					.setColor('#FFC0CB')
-				for (let mode in modes) {
-					if (modes[mode].value) {
-						embed.addField(mode, modes[mode].value ? "ON" : "OFF", true);
-					}
+				for (let mode in modes[message.guild.id]) {
+					embed.addField(mode, modes[message.guild.id][mode].value ? "ON" : "OFF", true);
 				}
-				if (embed.fields.length === 0) {
-					embed.setDescription("No modes toggled! Use >modes to see a list of available modes")
-				}
+				// if (embed.fields.length === 0) {
+				// 	embed.setDescription("No modes toggled! Use >modes to see a list of available modes")
+				// }
 				message.channel.send(embed);
 				break;
 			}
 			case 'toggle': {
 				let selectedMode;
-				for (let mode in modes) {
+				for (let mode in modesList) {
 					if (args.join(" ").toLowerCase() === mode.toLowerCase()) {
 						selectedMode = mode;
 					}
@@ -165,28 +211,80 @@ client.on('message', async message => {
 					message.channel.send(embed);
 					return;
 				}
-				modes[selectedMode].value = !modes[selectedMode].value;
+				let mode = modes[message.guild.id];
+				if (!mode) {
+					mode = modes[message.guild.id] = deepClone(modesList);
+				}
 				let embed = new Discord.MessageEmbed();
-					switch (selectedMode) {
-						case 'random': {
-							embed.setTitle("Random toggled " + (modes['random'].value ? "on" : "off") + "!");
-							embed.setDescription(modes['random'].value ? "Ditto will transform randomly without being prompted to." : "Ditto will no longer transform randomly.");
-							embed.setColor('#FFC0CB');
-							message.channel.send(embed);
-							let selected = Math.floor(Math.random() * people.people.length)
+				mode[selectedMode].value = !mode[selectedMode].value;
+				switch (selectedMode) {
+					case 'random': {
+						embed.setTitle("Random toggled " + (mode['random'].value ? "on" : "off") + "!");
+						embed.setDescription(mode['random'].value ? "Ditto will transform randomly without being prompted to." : "Ditto will no longer transform randomly.");
+						embed.setColor('#FFC0CB');
+						message.channel.send(embed);
+						let selected = Math.floor(Math.random() * people.people.length)
+						if (availableForms.includes(people.people[selected].id)) {
 							await transform(people.people[selected], message);
-							break;
 						}
+						break;
 					}
+					case 'exclusive': {
+						embed.setTitle("Exclusive toggled " + (mode['exclusive'].value ? "on" : "off") + "!");
+						embed.setDescription(mode['exclusive'].value ? "Ditto will only be able to transform into members in the server." : "Ditto will be able to transform into any form.");
+						embed.setColor('#FFC0CB');
+						message.channel.send(embed);
+						if (!availableForms[message.guild.id]) {
+							availableForms[message.guild.id] = [];
+						}
+						if (mode['exclusive'].value) {
+							clearArray(availableForms[message.guild.id]);
+							const users = [];
+							message.guild.members.cache.forEach(member => {
+								users.push(member.user);
+							});
+							for (let user of users) {
+								for (let person of people.people) {
+									if (person.id && person.id === user.id) {
+										availableForms[message.guild.id].push(person.id);
+									}
+								}
+							}
+						} else {
+							clearArray(availableForms[message.guild.id]);
+							for (let person of people.people) {
+								availableForms[message.guild.id].push(person.id);
+							}
+						}
+						break;
+					}
+				}
 				break;
 			}
 			case 'modes': {
 				let embed = new Discord.MessageEmbed()
 					.setTitle("Available modes:")
 					.setColor('#FFC0CB')
-				for (let mode in modes) {
-					embed.addField(mode, modes[mode].desc);
+				for (let mode in modesList) {
+					embed.addField(mode, modesList[mode].desc);
 				}
+				message.channel.send(embed);
+				break;
+			}
+			case 'rate': {
+				if (isNaN(args[0]) || args.length > 1 || parseFloat(args[0]) > 100 || parseFloat(args[0]) < 0) {
+					let embed = new Discord.MessageEmbed()
+						.setTitle(args.length === 0 ? "Current rate for " + message.channel.name + ": " + (rates[message.channel.id] * 100.0) + "%" : "Not a valid argument!")
+						.setDescription("Enter a percentage between 0-100 to set how likely the bot is to respond")
+						.setColor('#FFC0CB');
+					message.channel.send(embed);
+					return;
+				}
+				const rate = parseFloat(args[0]) / 100.0;
+				rates[message.channel.id] = rate;
+				let embed = new Discord.MessageEmbed()
+					.setTitle("Rate for " + message.channel.name + " set to " + parseFloat((rate * 100.0).toFixed(3)) + "%!")
+					.setColor('#FFC0CB');
 				message.channel.send(embed);
 				break;
 			}
@@ -202,11 +300,13 @@ client.on('message', async message => {
         let valid = false;
         for (let person of people.people) {
           if (args.join(" ").toLowerCase() === person.name.toLowerCase() || args[0] === person.mention || (person.id && args === person.id) || (person.username && args.join(" ").toLowerCase() === person.username.toLowerCase())) {
-						let currentForm = await transform(person, message);
-            // sendWebhook("Transformed into " + person.name + "!", transformed.nickname, transformed.avatar);
-            sendWebhook(currentForm.onTransform, currentForm.nickname, currentForm.avatar);
-            valid = true;
-            break;
+						if (availableForms[message.guild.id].includes(person.id)) {
+							let currentForm = await transform(person, message);
+	            // sendWebhook("Transformed into " + person.name + "!", transformed.nickname, transformed.avatar);
+	            sendWebhook(currentForm.onTransform, currentForm.nickname, currentForm.avatar);
+	            valid = true;
+	            break;
+						}
           }
         }
         if (!valid) {
@@ -242,14 +342,13 @@ async function transform(person, message) {
 	if (user !== "none") {
 		const member = message.guild.member(await client.users.fetch(person.id));
 		let nickname;
-		if (member.nickname) {
+		if (member && member.nickname) {
 			nickname = member.nickname;
 		} else {
 			nickname = user.username;
 		}
 		currentForm.nickname = nickname;
 	}
-	console.log(currentForm.nickname);
 	return currentForm;
 }
 
@@ -257,6 +356,10 @@ function clearArray(arr) {
 	while (arr.length > 0) {
 		arr.pop();
 	}
+}
+
+function deepClone(obj) {
+  return JSON.parse(JSON.stringify(obj));
 }
 
 async function sendWebhook(message, username, avatar) {
